b254571d5515410391ad8cb62f46846c6df135d9
harsh <harsh@codal.com>
2026-01-20 12:01:13 +0530

    Added inline edit and delete functions

--- Week2/todo-plain-js/script.js
+++ Week2/todo-plain-js/script.js
@@ -26,11 +26,30 @@ document.getElementById("delete-todo").addEventListener("click", (e) => {
 
 const todoListHTML = document.getElementById("todo-list");
 
+let todoToDeleteId = null;
+
 todoListHTML.addEventListener("click", (e) => {
   const deleteBtn = e.target.closest(".todo-delete-btn");
   if (!deleteBtn) return;
+
   const li = deleteBtn.closest("li");
-  deleteSingleTodo(li.id);
+  todoToDeleteId = li.id;
+
+  const modalEl = document.getElementById("deleteModal");
+  const modal = new bootstrap.Modal(modalEl);
+  modal.show();
+});
+
+// Confirm button inside modal
+document.getElementById("confirmDeleteBtn").addEventListener("click", () => {
+  if (todoToDeleteId) {
+    deleteSingleTodo(todoToDeleteId);
+    todoToDeleteId = null;
+  }
+
+  const modalEl = document.getElementById("deleteModal");
+  const modal = bootstrap.Modal.getInstance(modalEl);
+  modal.hide();
 });
 
 todoListHTML.addEventListener("click", (e) => {
